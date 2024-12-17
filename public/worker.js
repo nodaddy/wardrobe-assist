962711d5667c17c607362a45d3b8cfb31b12c9a5
Romita Sharma <romitasharma@Romitas-MacBook-Air.local>
2024-12-17 10:53:44 +0530

    service worker for cache

--- public/worker.js
+++ public/worker.js
@@ -31,18 +31,19 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = ['pwa-task-manager-v2']; // Update cache version
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (!cacheWhitelist.includes(cacheName)) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
