4cad99fa40dc59c75821faeaa1773e63d9593d00
Romita Sharma <romitasharma@Romitas-MacBook-Air.local>
2025-01-26 21:01:00 +0530

    app short name changed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -37,16 +37,85 @@ function LandingPage() {
             fontFamily: 'Poppins, sans-serif',
           }}
         >
+          <>
+          <div
+        style={{
+          backgroundColor: 'white',
+          color: '#fff',
+          width: '100vw',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          fontFamily: 'Poppins, sans-serif',
+        }}
+      >
+        <img src={logo} alt="Logo" style={{ width: '100px' }} />
+        <div>
+          <button
+            style={{
+              backgroundColor: '#fff',
+              color: '#fb9a7a',
+              padding: '8px 15px',
+              border: '1px solid #fb9a7a',
+              borderRadius: '20px',
+              fontSize: '0.9rem',
+              fontWeight: 'bold',
+              cursor: 'pointer',
+              marginRight: '10px',
+              transition: 'transform 0.2s',
+            }}
+            onMouseOver={(e) => {
+              e.target.style.transform = 'scale(1.1)';
+            }}
+            onMouseOut={(e) => {
+              e.target.style.transform = 'scale(1)';
+            }}
+            onClick={() => {
+              if (localStorage.getItem('user')) {
+                localStorage.removeItem('user');
+                navigate('/');
+              } else {
+                setSigningIn(true);
+                signInWithPopup(auth, provider)
+                  .then((result) => {
+                    const user = result.user;
+                    localStorage.setItem('user', JSON.stringify(user));
+                    console.log(user);
+                    window.location.reload();
+                  })
+                  .catch((error) => {
+                    console.error(error);
+                    setSigningIn(false);
+                  });
+              }
+            }}
+          >{localStorage.getItem('user') ? (
+              `Hi, ${JSON.parse(localStorage.getItem('user')).displayName.split(' ')[0]}`
+            ) : (
+              <span style={{display: 'flex', alignItems: 'center'}}>
+                <GoogleOutlined /> {signingIn ? <>&nbsp;<Spin indicator={<Loading3QuartersOutlined style={{ fontSize: 15, color: 'grey'}} spin />} /></> : <>&nbsp; Sign in with Google &nbsp;</>}
+              </span>
+            )}
+            
+          </button>
+
+          &nbsp;
+            &nbsp;
+            &nbsp;
+        </div>
+      </div>
+          
           <div style={{ textAlign: 'center' }}>
+            {/* <img src={logo} alt="Logo" style={{ width: '150px', marginBottom: '-10px' }} /> */}
             <br/>
-            <img src={logo} alt="Logo" style={{ width: '150px', marginBottom: '-10px' }} />
+            <br/>
             <h1 style={{ fontSize: '2.2rem', fontWeight: '500' }}>Unlock Your Fashion Potential</h1>
             <p style={{ fontSize: '1.2rem', marginTop: '10px', lineHeight: '1.5' }}>
               Discover endless outfit possibilities and fashion advice.
               <br /> End indecisiveness with just a few taps.
             </p>
-            <div style={{ marginTop: '30px' }}>
-              <button
+            <div style={{ marginTop: '0px' }}>
+              {/* <button
                 style={{
                   background: '#ffffff',
                   color: '#fb9a7a',
@@ -92,20 +161,18 @@ function LandingPage() {
                     <GoogleOutlined /> {signingIn ? <Spin indicator={<Loading3QuartersOutlined spin />} /> : 'Sign in with Google'}
                   </span>
                 )}
-              </button>
-              <br/> 
+              </button> */}
             </div>
           </div>
-          
+          </>
           <div style={{
             marginTop: '50px',
             backgroundColor: '#fff',
             padding: '40px 20px',
-            borderRadius: '15px 15px 0 0',
             textAlign: 'center',
             boxShadow: '0 -4px 10px rgba(0, 0, 0, 0.1)',
           }}>
-            <h2 style={{ color: '#fb9a7a', fontSize: '1.7rem', fontWeight: '500' }}>What Our Users Say</h2>
+            <h2 style={{ color: '#fb9a7a', fontSize: '1.5rem', fontWeight: '500' }}>What Our Users Say</h2>
             <div style={{
               display: 'flex',
               justifyContent: 'space-around',
@@ -136,7 +203,7 @@ function LandingPage() {
                 <p style={{ fontStyle: 'italic', color: '#555' }}>
                   "Amazing tool! It helped me decide what to wear for my big presentation. Highly recommend."
                 </p>
-                <h4 style={{ color: '#fb9a7a', marginTop: '10px' }}>- James P.</h4>
+                <h4 style={{ color: '#fb9a7a', marginTop: '10px' }}>- James Peterson</h4>
               </div>
               <div style={{
                 maxWidth: '300px',
@@ -149,7 +216,7 @@ function LandingPage() {
                 <p style={{ fontStyle: 'italic', color: '#555' }}>
                   "Drobe is my go-to app for fashion advice. The suggestions are always on point!"
                 </p>
-                <h4 style={{ color: '#fb9a7a', marginTop: '10px' }}>- Emily R.</h4>
+                <h4 style={{ color: '#fb9a7a', marginTop: '10px' }}>- Emily Rocher</h4>
               </div>
             </div>
           </div>
